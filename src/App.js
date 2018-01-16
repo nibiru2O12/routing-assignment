cdcb38c2614382ecc4b74bb268c2f1134ea44254
rjgagui <robert.gaguitr@gmail.com>
2018-01-16 20:17:25 +0800

    added NavLink on course list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import {BrowserRouter,Route,Redirect} from 'react-router-dom';
+import {BrowserRouter,Route,Redirect,Switch} from 'react-router-dom';
 
 import Layout from './containers/Layout/Layout';
 import Courses from './containers/Courses/Courses';
+import Course from './containers/Course/Course';
 import Users from './containers/Users/Users';
 
 class App extends Component {
@@ -10,9 +11,12 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Layout>
-          <Route path="/users" component={Users}/>
-          <Route path="/courses" component={Courses}/>
-          <Redirect from='/' to='/courses' />
+          <Switch>
+            <Route path="/users" component={Users}/>
+            <Route path="/courses" component={Courses}/>
+            <Route path="/course/:id" component={Course}/>
+            <Redirect from='/' to='/courses' />
+          </Switch>
         </Layout>
       </BrowserRouter>
     );
--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './Courses.css';
+import { NavLink } from 'react-router-dom';
 
 class Courses extends Component {
     state = {
@@ -12,15 +13,24 @@ class Courses extends Component {
     }
 
     render () {
+
+        //const {url} = this.props.match;
+        
+        const courses = this.state.courses.map( course => {
+            return (
+                    <NavLink to={ '/course/' + course.id } key={course.id}>
+                        <article className="Course" key={course.id}>
+                            {course.title}
+                        </article>
+                    </NavLink>
+            );
+        } );
+
         return (
             <div>
                 <h1>Amazing Udemy Courses</h1>
                 <section className="Courses">
-                    {
-                        this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
-                        } )
-                    }
+                    {courses}
                 </section>
             </div>
         );
